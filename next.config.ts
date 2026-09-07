91a0f279d635b15ce589a6bf0dc4f9811f49e4cf
eric <eric.brilliant@gmail.com>
2026-09-07 14:18:37 +0800

    keep to debug sharp mac

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.31.31"],
-  outputFileTracingIncludes: {
-    "/*": [
-      "./node_modules/@img/sharp-linux-x64/**/*",
-      "./node_modules/@img/sharp-libvips-linux-x64/**/*",
-    ],
-  },
 };
 
 export default nextConfig;
